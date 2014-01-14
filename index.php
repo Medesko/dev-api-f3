a1323174e829f0f1cbc0a6d7051197a907cac137
Mohamed KEITA <medesko@gmail.com>
2014-01-14 10:11:24 +0100

    new architecture

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
 <?php
 
-$f3=require('lib/base.php');
+$f3=require('core/base.php');
 
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
 
-$f3->config('ini/config.ini');
+$f3->config('api/configs/config.ini');
 
 $f3->route('GET /',
 	function($f3) {
 		$f3->set('content','index.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
-
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
 		echo View::instance()->render('layout.htm');
 	}
 );
